0b7c922b74ab1db4c7fa8fcc6b9962cca8b21119
elouannasse <elouannassez@gmail.com>
2024-11-26 18:18:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let myitem2 = document.getElementById("item2");
+let mytext = document.createElement("p");
+mytext.style.color = "red";
+mytext.textContent = "dsgqfeqf";
+myitem2.appendChild(mytext);
+console.log(myitem2);
