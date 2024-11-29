d659f86d378ebee8c7077a92f2b58ffa01e11aaf
elouannasse <elouannassez@gmail.com>
2024-11-29 20:22:23 +0100

    j'ai travailler sur la fonction mofifier

--- script.js
+++ script.js
@@ -1,137 +1,122 @@
-let classatru = document.getElementsByClassName("iteam");
-console.log(classatru);
-let myposition = document.getElementById("position");
-let myajou = document.getElementById("ajou");
-let myname = document.getElementById("name");
-let myphoto = document.getElementById("photo");
-let mynationality = document.getElementById("nationality");
-let myflag = document.getElementById("flag");
-let myclub = document.getElementById("club");
-let mylogo = document.getElementById("logo");
-let myrating = document.getElementById("rating");
-let mypace = document.getElementById("pace");
-let myshooting = document.getElementById("shooting");
-let mypassing = document.getElementById("passing");
-let mydribbling = document.getElementById("dribbling");
-let mydefending = document.getElementById("defending");
-let myphysical = document.getElementById("physical");
+document.addEventListener("DOMContentLoaded", () => {
+  // Définition des éléments du formulaire
+  const formElements = {
+    position: document.getElementById("position"),
+    name: document.getElementById("name"),
+    photo: document.getElementById("photo"),
+    nationality: document.getElementById("nationality"),
+    flag: document.getElementById("flag"),
+    club: document.getElementById("club"),
+    logo: document.getElementById("logo"),
+    rating: document.getElementById("rating"),
+    pace: document.getElementById("pace"),
+    shooting: document.getElementById("shooting"),
+    passing: document.getElementById("passing"),
+    dribbling: document.getElementById("dribbling"),
+    defending: document.getElementById("defending"),
+    physical: document.getElementById("physical"),
+    btnn: document.getElementById("btnn"),
+    btn1: document.getElementsByClassName("btn1"),
+    btn2: document.getElementsByClassName("btn2"),
+  };
 
-function createPlayerCard() {
-  return `
+  function createPlayerCard(data) {
+    return `
+        <div id="btnn">
+            <button class="btn1">🖊️</button>
+            <button class="btn2">🗑️</button>
+        </div>
         <div class="player-card">
             <div style="width: 100px; margin-right: 6px">
                 <div style="display: flex; gap: 1px">
                     <div style="display: flex; flex-direction: column; margin-top: 25px">
-                        <span style="color: white; font-weight: bold; font-size: 25px">${myrating.value}</span>
-                        <span style="color: white; font-weight: bold; font-size: 15px">${myposition.value}</span>
+                        <span style="color: white; font-weight: bold; font-size: 25px">${data.rating.value}</span>
+                        <span style="color: white; font-weight: bold; font-size: 15px">${data.position.value}</span>
                     </div>
-                    <img src="${myphoto.value}" style="width: 60px; height: 60px; margin-top: 15px" alt="${myname.value}" />
+                    <img src="${data.photo.value}" style="width: 60px; height: 60px; margin-top: 15px" alt="${data.name.value}" />
                 </div>
                 <div>
                     <div>
-                        <span style="color: white; font-weight: bold; font-size: 19px">${myname.value}</span>
+                        <span style="color: white; font-weight: bold; font-size: 19px">${data.name.value}</span>
                     </div>
                     <div style="display: flex; gap: 3px">
                         <div style="display: flex; flex-direction: column; align-items: center">
-                            <span style="color: white; font-weight: bold; font-size: 9px">DRI</span>
-                            <span style="color: white; font-weight: bold; font-size: 9px">${mydribbling.value}</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">Dribble</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">${data.dribbling.value}</span>
                         </div>
                         <div style="display: flex; flex-direction: column; align-items: center">
-                            <span style="color: white; font-weight: bold; font-size: 9px">DEF</span>
-                            <span style="color: white; font-weight: bold; font-size: 9px">${mydefending.value}</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">Défense</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">${data.defending.value}</span>
                         </div>
                         <div style="display: flex; flex-direction: column; align-items: center">
-                            <span style="color: white; font-weight: bold; font-size: 9px">PHY</span>
-                            <span style="color: white; font-weight: bold; font-size: 9px">${myphysical.value}</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">Physique</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">${data.physical.value}</span>
                         </div>
                         <div style="display: flex; flex-direction: column; align-items: center">
-                            <span style="color: white; font-weight: bold; font-size: 9px">PAC</span>
-                            <span style="color: white; font-weight: bold; font-size: 9px">${mypace.value}</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">Vitesse</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">${data.pace.value}</span>
                         </div>
                         <div style="display: flex; flex-direction: column; align-items: center">
-                            <span style="color: white; font-weight: bold; font-size: 9px">SHOT</span>
-                            <span style="color: white; font-weight: bold; font-size: 9px">${myshooting.value}</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">Tir</span>
+                            <span style="color: white; font-weight: bold; font-size: 9px">${data.shooting.value}</span>
                         </div>
                     </div>
                     <div style="display: flex; align-items: center; justify-content: center; gap: 10px">
-                        <img src="${myflag.value}" style="height: 20px; width: 20px" alt="${mynationality.value}" />
-                        <img src="${mylogo.value}" style="height: 20px; width: 20px" alt="${myclub.value}" />
+                        <img src="${data.flag.value}" style="height: 20px; width: 20px" alt="${data.nationality.value}" />
+                        <img src="${data.logo.value}" style="height: 20px; width: 20px" alt="${data.club.value}" />
                     </div>
                 </div>
             </div>
         </div>
     `;
-}
-
-myajou.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  let cmPositions = Array.from(classatru).filter(
-    (item) => item.getAttribute("jouer_cm") !== null
-  );
-
-  for (let i = 0; i < Array.from(classatru).length; i++) {
-    let GK = classatru[i].getAttribute("gardien_gk");
-    let LB = classatru[i].getAttribute("jouer_LB");
-    let CB = classatru[i].getAttribute("jouer_cb");
-    let RB = classatru[i].getAttribute("jouer_rb");
-    let CM = classatru[i].getAttribute("jouer_cm");
-    let LW = classatru[i].getAttribute("jouer_lw");
-    let ST = classatru[i].getAttribute("jouer_st");
-    let RW = classatru[i].getAttribute("jouer_rw");
-
-    if (myposition.value === "GK" && GK !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "LB" && LB !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "CB" && CB !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "RB" && RB !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "CM") {
-      if (cmPositions[0] && !cmPositions[0].querySelector(".player-card")) {
-        cmPositions[0].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "CDM") {
-      if (cmPositions[1] && !cmPositions[1].querySelector(".player-card")) {
-        cmPositions[1].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "CAM") {
-      if (cmPositions[2] && !cmPositions[2].querySelector(".player-card")) {
-        cmPositions[2].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "LW" && LW !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "ST" && ST !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    } else if (myposition.value === "RW" && RW !== null) {
-      if (!classatru[i].querySelector(".player-card")) {
-        classatru[i].innerHTML = createPlayerCard();
-        break;
-      }
-    }
   }
 
-  document.getElementById("playerForm").reset();
+  document.getElementById("playerForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (formElements.position.value === "CB") {
+      const cbElements = document.querySelectorAll(".iteam[jouer_cb]");
+      for (let cb of cbElements) {
+        if (!cb.querySelector(".player-card")) {
+          cb.innerHTML = createPlayerCard(formElements);
+          break;
+        }
+      }
+      e.target.reset();
+      return;
+    }
+
+    if (["CM", "CDM", "CAM"].includes(formElements.position.value)) {
+      const positionMapping = { CM: 0, CDM: 1, CAM: 2 };
+      const index = positionMapping[formElements.position.value];
+      const cmElements = document.querySelectorAll(".iteam[jouer_cm]");
+
+      if (!cmElements[index].querySelector(".player-card")) {
+        cmElements[index].innerHTML = createPlayerCard(formElements);
+      }
+      e.target.reset();
+      return;
+    }
+
+    const positionMapping = {
+      GK: "gardien_gk",
+      LB: "jouer_LB",
+      RB: "jouer_rb",
+      LW: "jouer_lw",
+      ST: "jouer_st",
+      RW: "jouer_rw",
+    };
+
+    const position = formElements.position.value;
+    const attribute = positionMapping[position];
+
+    if (attribute) {
+      const element = document.querySelector(`.iteam[${attribute}]`);
+      if (element && !element.querySelector(".player-card")) {
+        element.innerHTML = createPlayerCard(formElements);
+      }
+    }
+
+    e.target.reset();
+  });
 });
